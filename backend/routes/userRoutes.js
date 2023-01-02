fb7aca2479114921dc062208fc81970e7ece5b89
m7madnat <m7madnatseh1999@gmail.com>
2023-01-02 23:08:51 +0200

    new

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
-import data from "../data.js";
 import User from "../models/userModel.js";
 import { generateToken, isAdmin, isAuth } from "../utils.js";
 
@@ -17,15 +16,6 @@ userRouter.get(
     res.send(topSellers);
   })
 );
-// seed users
-// userRouter.get(
-//   "/seed",
-//   expressAsyncHandler(async (req, res) => {
-//     // await User.remove({});
-//     const createdUsers = await User.insertMany(data.users);
-//     res.send({ createdUsers });
-//   })
-// );
 // signin
 userRouter.post(
   "/signin",
